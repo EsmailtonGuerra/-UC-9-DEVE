f809f7954a8fce44cd3148c3555f7059236f175b
Esmailton G. F.tas <75494686+EsmailtonGuerra@users.noreply.github.com>
2025-05-13 21:19:41 -0300

    Create  index.html  - style.css  - script.js
    Boas PráticasO que é?São práticas que garantem um desenvolvimento de software eficiente, com
    qualidade, legibilidade e foco nas necessidades do cliente, facilitando a
    manutenção e reduzindo erros.

---
+++ index.html  - style.css  - script.js
@@ -0,0 +1,4 @@
+
+Descrição: 
+Este exercício solicita ao usuário que digite um novo nome em um campo de texto. Ao clicar no botão
+'Atualizar', o nome exibido na tela será alterado com base no valor digitado.  
